f0cc3079ae660d027ca399280aa05ac824c61c4c
Dave Sanders <davepaulsanders@gmail.com>
2022-05-06 14:31:11 -0400

    Added .env and database mysql2 conection

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
-const { ppid } = require("process");
 const PORT = process.env.PORT || 3001;
-
+const mysql = require("mysql2");
+require("dotenv").config();
+const password = process.env.MYSQL_KEY;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
